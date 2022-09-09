888bbaed0f2c1ee6fd74634d3afbd41c96bbd841
Alyson Tzou <alysontzou@gmail.com>
2022-09-09 17:35:41 -0400

    new problem CC9-9-2022

--- CC9-9-2022/main.js
+++ CC9-9-2022/main.js
@@ -1,14 +1,33 @@
+//========= PROBLEM 1 ==========//
 //Create a function that takes an integer as an argument and returns "Even" 
 //for even numbers or "Odd" for odd numbers.
 
-//=====WORK=========//
+//=====WORK======//
 //P: 1 param
 //R: return even for even num and odd for odd num
 //E: N/A
 //P: 
     //return numbers divisible by 2 with no remainder as even
     //rest is odd
-    
+
     function even_or_odd(number) {
         return(number%2 == 0 ? "Even" : "Odd");
       }
+
+//======== PROBLEM 2 ==========//
+
+// Create a function called shortcut to remove the lowercase vowels (a, e, i, o, u ) in a given string.
+
+//==== WORK ========//
+//P: 1 param
+//R: return struing with removed vowels
+//E: 
+    //"hello"     -->  "hll"
+    // "goodbye"   -->  "gdby"
+    // "HELLO"     -->  "HELLO"
+//P: 
+    //replace vowels in string with '' (remove vowels)
+
+    function shortcut(string){
+        return string.replace(/[aeiou]/g,'') //aeiou are words being plucked from string
+    }
