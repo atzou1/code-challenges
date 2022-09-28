75e1d1f896ca0ed6143eff93ace16b1f7d65b1e8
Alyson Tzou <alysontzou@gmail.com>
2022-09-27 22:41:41 -0400

    completed another problem CC9-27-2022

--- CC9-27-2022/main.js
+++ CC9-27-2022/main.js
@@ -9,3 +9,21 @@
 function doubleChar(str) {
     return str.split('').map(x => x.repeat(2)).join('')
   }
+
+
+  
+//=========================================================//
+// Replace all vowel to exclamation mark in the sentence. aeiouAEIOU is vowel.
+
+// Examples
+// replace("Hi!") === "H!!"
+// replace("!Hi! Hi!") === "!H!! H!!"
+// replace("aeiou") === "!!!!!"
+// replace("ABCDE") === "!BCD!"
+
+//======== WORK==========//
+//find all vowels (aeiou) and replace them with !
+
+function replace(s){
+  return s.replace(/[aeiou]/gi, '!');
+}
