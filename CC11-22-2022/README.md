b5a37b4cce582d5415f713efa1e05400d6939dca
Alyson Tzou <alysontzou@gmail.com>
2022-11-22 15:08:21 -0500

    CC11-22-2022

--- CC11-22-2022/README.md
+++ CC11-22-2022/README.md
@@ -3,3 +3,8 @@
 Returns a negative number when number is less than 0. If number is 0 or less than 0, it returns the original number
 
 //CHALLENGE 2
+Returns a function that accepts a string parameter, and reverses each word in the string. All spaces in the string should be retained.
+
+// Examples
+// "This is an example!" ==> "sihT si na !elpmaxe"
+// "double  spaces"      ==> "elbuod  secaps"
--- CC11-22-2022/main.js
+++ CC11-22-2022/main.js
@@ -1,3 +1,4 @@
+//===========================CHALLENGE 1================//
 // In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
 
 // Examples
@@ -6,6 +7,21 @@
 // makeNegative(0);    // return 0
 // makeNegative(0.12); // return 
 
-//===========WORK===========//
+//===========WORK=======//
 //make conditional if > 0, return negative else return num
 let makeNegative = num => num > 0 ? -num : num;
+
+//=============================CHALLENGE 2=======================//
+// Complete the function that accepts a string parameter, and reverses each word in the string. All spaces in the string should be retained.
+
+// Examples
+// "This is an example!" ==> "sihT si na !elpmaxe"
+// "double  spaces"      ==> "elbuod  secaps"
+
+//=======WORK=========//
+//split,reverse,join to reverse words and letters
+//split, reverse, join again to put word position back to original position, but keep words reverse
+
+function reversedWords(str){
+    return str.split('').reverse().join('').split(' ').reverse().join(' ');
+}
