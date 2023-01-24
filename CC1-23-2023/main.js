0169e7ac1e69bce824436c2ebd7ff2614e9dd50e
Alyson Tzou <alysontzou@gmail.com>
2023-01-23 23:21:49 -0500

    CC1-23-2022

---
+++ CC1-23-2023/main.js
@@ -0,0 +1,10 @@
+//create a function that returns numbers in an array that are even
+//create param numbersArray
+//return a filter that grabs even numbers'
+
+
+function getEvenNumbers(numbersArray){
+    return numbersArray.filter(x => x % 2 === 0)
+}
+
+
